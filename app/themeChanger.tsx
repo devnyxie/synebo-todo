6a541389e8e9f1b33cf9d4a7f8b1aeda82c1073e
Tim Afanasiev <soupsoupchannel@gmail.com>
2024-07-30 15:33:35 +0200

    default-sys-theme icon added

--- app/themeChanger.tsx
+++ app/themeChanger.tsx
@@ -15,13 +15,21 @@ const ThemeChanger = () => {
       setIcon("/synebo-todo/icon-sun.svg");
     } else if (theme === "light") {
       setIcon("/synebo-todo/icon-moon.svg");
+    } else {
+      setIcon("/synebo-todo/icon-system.svg");
     }
   }, [theme]);
 
   return (
     <div>
       <button onClick={() => setTheme(theme == "light" ? "dark" : "light")}>
-        <Image src={icon} alt="Toggle Color Mode" width={24} height={24} />
+        <Image
+          src={icon}
+          alt="Toggle Color Mode"
+          width={24}
+          height={24}
+          className="text-white"
+        />
       </button>
     </div>
   );
