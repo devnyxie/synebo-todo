570f34412cb2796e239def9b66f6e83d5f746152
Tim Afanasiev <soupsoupchannel@gmail.com>
2024-07-30 13:26:27 +0200

    dnd implemented

--- app/ToDoItem.tsx
+++ app/ToDoItem.tsx
@@ -1,26 +1,92 @@
 "use client";
 import React, { useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface TodoItemProps {
+  id: number;
   text: string;
+  done?: boolean;
+  onDelete?: (id: number) => void;
+  onAdd?: (text: string, checked: boolean) => void;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ text }) => {
-  const [isChecked, setIsChecked] = useState(false);
+const TodoItem: React.FC<TodoItemProps> = ({
+  id,
+  text,
+  done,
+  onDelete,
+  onAdd,
+}) => {
+  const [isChecked, setIsChecked] = useState(done ?? false);
   const [inputText, setInputText] = useState(text);
 
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && onAdd) {
+      onAdd(inputText, isChecked);
+      setInputText("");
+    }
+  };
+
+  const isCursorGrabbing = attributes["aria-pressed"];
+
   return (
-    <div className="todo-item w-full bg flex p-4">
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="todo-item w-full bg flex p-4 items-center"
+    >
+      <label className="custom-radio mr-2">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+        <span className="checkmark"></span>
+      </label>
       <input
         type="text"
         value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
         style={{
           textDecoration: isChecked ? "line-through" : "none",
         }}
         className="w-full"
       />
-      {/* <button onClick={onDelete}>×</button> */}
+      {onDelete && (
+        <button onClick={() => onDelete(id)} className="ml-2">
+          ×
+        </button>
+      )}
+      {id !== 0 && (
+        <button
+          {...attributes}
+          {...listeners}
+          className={`ml-2 ${
+            isCursorGrabbing ? "cursor-grabbing" : "cursor-grab"
+          }`}
+        >
+          <svg viewBox="0 0 20 20" width="15">
+            <path
+              d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,102 @@
 "use client";
+import Image from "next/image";
+import banner from "@/public/bg-desktop-light.jpeg";
 import TodoItem from "./ToDoItem";
 import { useState } from "react";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import {
+  restrictToVerticalAxis,
+  restrictToParentElement,
+} from "@dnd-kit/modifiers";
 
 interface ToDoItem {
+  id: number;
   text: string;
   done: boolean;
 }
 
 export default function Home() {
   const [todos, setTodos] = useState<ToDoItem[]>([
-    { text: "Complete online JavaScript course", done: false },
-    { text: "Jog around the park 3x", done: false },
-    { text: "10 minutes meditation", done: false },
-    { text: "Read for 1 hour", done: false },
-    { text: "Pick up groceries", done: false },
-    { text: "Complete Todo App on Frontend Mentor", done: false },
+    { id: 1, text: "Complete online JavaScript course", done: false },
+    { id: 2, text: "Jog around the park 3x", done: false },
+    { id: 3, text: "10 minutes meditation", done: false },
+    { id: 4, text: "Read for 1 hour", done: false },
+    { id: 5, text: "Pick up groceries", done: false },
+    { id: 6, text: "Complete Todo App on Frontend Mentor", done: false },
   ]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
   const itemsLeft = todos.filter((todo) => !todo.done).length;
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      setTodos((prevTodos) => {
+        const oldIndex = prevTodos.findIndex((todo) => todo.id === active.id);
+        const newIndex = prevTodos.findIndex((todo) => todo.id === over.id);
+        return arrayMove(prevTodos, oldIndex, newIndex);
+      });
+    }
+  }
+
+  function handleDelete(idToDelete: number) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== idToDelete));
+  }
+
+  function addNewTodo(newTodoText: string, checked: boolean) {
+    const newId = Math.max(...todos.map((todo) => todo.id), 0) + 1;
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: newId, text: newTodoText, done: checked ?? false },
+    ]);
+  }
+
   return (
     <div className="container mx-auto max-w-4xl px-4 pt-20">
       <h1 className="text-4xl font-bold text-white">TODO</h1>
       {/* add tasks input */}
       <div className="mt-4 mb-4 rounded-md overflow-hidden">
-        <TodoItem text="Your new task" />
+        <TodoItem id={0} text="Your new task" onAdd={addNewTodo} />
       </div>
-
       {/* all tasks */}
-      <div className="rounded-md overflow-hidden shadow-md divide-y divide-gray-200 dark:divide-gray-600">
-        {todos.map((todo, index) => (
-          <TodoItem key={index} text={todo.text} />
-        ))}
+      <div className="bg rounded-md overflow-hidden shadow-md divide-y divide-gray-200 dark:divide-gray-600">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+          modifiers={[restrictToVerticalAxis, restrictToParentElement]}
+        >
+          <SortableContext items={todos} strategy={verticalListSortingStrategy}>
+            {todos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                id={todo.id}
+                text={todo.text}
+                done={todo.done}
+                onDelete={handleDelete}
+              />
+            ))}
+          </SortableContext>
+        </DndContext>
         <div className="p-4 flex w-full justify-between text-base">
           <p>{itemsLeft} items left</p>
           <div className="flex space-x-2">
